8053afb9ba0ad3dae06e79ba86d992a2bf806b5b
Sagnik Santra <sagnik.santra17@gmail.com>
2026-03-26 08:45:48 +0530

    Added the edit task name option

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ while True:
     print("1: View tasks")
     print("2: Add task")
     print("3: Delete task")
+    print("4: Update task")
     print("10: Exit")
     print()
 
@@ -46,7 +47,7 @@ while True:
 
         if 1 <= user_input <= 11:
 
-            if user_input == 2:
+            if user_input == 2: #Add task
 
                 if current_user:
                     new_task_name = input("Enter task name: ")  
@@ -58,12 +59,11 @@ while True:
                     current_user["tasks"].append(task.to_dict())
                     data.save_data(existing_users)
 
-            elif user_input == 1:
+            elif user_input == 1: #View tasks
                 if not current_user["tasks"]:
                     print("No tasks available")
                 else:
                     for task_number, task_item in enumerate(current_user["tasks"], start=1):
-
                         print(f"Task Id: {task_number}")    
                         print(f"{utils.main_task_func(task_item)}\n")
 
@@ -84,7 +84,7 @@ while True:
                         except ValueError:
                             print("Wrong input! Please pick again.")
             
-            elif user_input == 3:
+            elif user_input == 3: #Delete task
                 if not current_user["tasks"]:
                     print("No task to delete")
                     print()
@@ -92,7 +92,6 @@ while True:
                 else:
                     while True:
                         for task_number, task_item in enumerate(current_user["tasks"], start=1):
-
                             print(f"Task Id: {task_number}")    
                             print(f"{utils.main_task_func(task_item)}\n")
                     
@@ -111,12 +110,62 @@ while True:
                         except ValueError:
                             print("Please pick a valid Id: ")
 
+            elif user_input == 4: #Edit tasks
+                if not current_user["tasks"]:
+                    print("No task to edit")
+                    print()
+
+                else:
+                    print("Which task do you want to edit?")
+                    print()
+                    for task_number, task_item in enumerate(current_user["tasks"], start=1):
+                            print(f"Task Id: {task_number}")    
+                            print(f"{utils.main_task_func(task_item)}\n")
+
+                    while True:
+
+                        try:
+                            edit_task_id = int(input("Enter task id: "))
+    
+                            if  1 <= edit_task_id <= len(current_user["tasks"]):
+                                print()
+                                print("What do you want to edit?")
+                                print("1. Edit task name")
+                                print("2. Edit task category")
+                                print("3. Edit task description")
+                                print("4. Edit task creation date")
+                                print("5. Edit task due date")
+                                print("6. Edit task completion date")
+                                print()
+
+
+                                try:
+                                    edit_action_num = int(input("Choose an action: "))
+
+                                    if 1 <= edit_action_num <= 6:
+                                        if edit_action_num == 1:
+                                            edited_task_name = input("Enter the modified task name: ")
+
+                                            current_user["tasks"][edit_task_id -1]["task_name"] = edited_task_name
+                                            print("Task name upadated successfully!")
+                                            data.save_data(existing_users)
+                                            break
+                                        
+                                        
+
+                                    else:
+                                        print("Please pick a valid action!")
+
+
+
+
+                                except ValueError:
+                                    print("Invalid input! Please enter a valid input.")
                 
+                        except ValueError:
+                            print("Invalid input! Please enter a valid input.")
 
-                
-
-
-            elif user_input == 10:
+            elif user_input == 10: #Exit
                 break
 
         else:
